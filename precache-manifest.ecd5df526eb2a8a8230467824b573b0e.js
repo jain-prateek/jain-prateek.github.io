fc5f9bd6d1fe5ff49446797380605de27d9ec05c
Jain <703232732@GENPACT.COM>
2019-07-13 12:40:53 +0530

    adding new templates

---
+++ precache-manifest.ecd5df526eb2a8a8230467824b573b0e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee9108389cdcaebe0750e8dae7158f13",
+    "url": "/static/media/bg.ee910838.jpg"
+  },
+  {
+    "revision": "73bc78539fcdf0eb980c286ef702c1c1",
+    "url": "/static/media/RanvijaySachan.73bc7853.jpg"
+  },
+  {
+    "revision": "154d5f326bc974dadcbba627fe5792fa",
+    "url": "/static/media/Ranvijay Sachan - Resume.154d5f32.pdf"
+  },
+  {
+    "revision": "b6fbbf4e25b32aa991f5fd52a65babdc",
+    "url": "/static/media/Ranvijay Sachan - Coverletter.b6fbbf4e.pdf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d9af6794cf8ec4181be3",
+    "url": "/static/js/main.d9af6794.chunk.js"
+  },
+  {
+    "revision": "82c96b85f52197d8d434",
+    "url": "/static/js/2.82c96b85.chunk.js"
+  },
+  {
+    "revision": "d9af6794cf8ec4181be3",
+    "url": "/static/css/main.2629f91f.chunk.css"
+  },
+  {
+    "revision": "c9967e21d3f2c50d5588ffcd73a1a07d",
+    "url": "/index.html"
+  }
+];
